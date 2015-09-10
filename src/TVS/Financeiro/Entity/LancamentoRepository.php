3289a2fff3f3ee52cb07091167ccd957ec4adcd5
Thiago Santos <thiago_vick@yahoo.com.br>
2015-09-10 13:23:57 -0300

    Ajustando exibição somente dos registros do mês de referencia

--- src/TVS/Financeiro/Entity/LancamentoRepository.php
+++ src/TVS/Financeiro/Entity/LancamentoRepository.php
@@ -3,26 +3,105 @@
 namespace TVS\Financeiro\Entity;
 
 use Doctrine\ORM\EntityRepository;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 class LancamentoRepository extends EntityRepository{
+    private $session;
     
-    use \TVS\Base\Traits\Repository;
-    
-    public function lancamentosGeralMes() {
-        $session = new Session();
-        $user = $session->get('user');
-        $baseDate = $session->get('baseDate');
-        $query = $this->createQueryBuilder('p')
-                ->where('((p.vencimento >= :dateBaseIni and p.vencimento <= :dateBaseFim) or (p.vencimento < :dateBaseIni and p.pagamento is null) or (p.pagamento >= :dateBaseIni and p.pagamento <= :dateBaseFim)) and p.user = :user')
-                ->setParameters(array(
-                    'dateBaseIni'=>$baseDate."-01",
-                    'dateBaseFim'=>$baseDate."-31",
-                    'user' => $user->getId()
-                ))
-                ->orderBy('p.pagamento,p.vencimento', 'ASC')
-                ->getQuery();
-        //\Zend\Debug\Debug::dump($query->getSQL());
-        //\Zend\Debug\Debug::dump($container->user->getId());
-        return $query->getResult();
+    public function getSession() {
+        if($this->session instanceof Session){
+            return $this->session;
+        }
+        return $this->session = new Session();
     }
+    
+    public function findPagination($firstResult, $maxResults, $user = false) {
+        $session = $this->getSession();
+        $base_date = $session->get('baseDate');
+        $query = $this->createQueryBuilder('c');
+        if ($user) {
+            $query->where("(c.competencia >= :date_start and c.competencia <= :date_end) and c.user = :user")
+                    ->setParameters(array(
+                    'date_start'=>$base_date."-01",
+                    'date_end'=>$base_date."-31",
+                    'user' => $user
+                ));
+        }
+        return $query->setFirstResult($firstResult)
+                        ->setMaxResults($maxResults)
+                        ->orderBy('c.pagamento,c.vencimento', 'ASC')
+                        ->getQuery()
+                        ->getResult();
+    }
+
+    public function getRows($user = false) {
+        $session = $this->getSession();
+        $base_date = $session->get('baseDate');
+        $query = $this->createQueryBuilder('c')
+                ->select('Count(c)');
+        if ($user) {
+             $query->where("(c.competencia >= :date_start and c.competencia <= :date_end) and c.user = :user")
+                    ->setParameters(array(
+                    'date_start'=>$base_date."-01",
+                    'date_end'=>$base_date."-31",
+                    'user' => $user
+                ));
+        }
+        return $query->getQuery()
+                        ->getSingleScalarResult();
+    }
+
+    public function findSearch($firstResult, $maxResults, $search, $field, $user = false) {
+        $session = $this->getSession();
+        $base_date = $session->get('baseDate');
+        $query = $this->createQueryBuilder('c')
+                    ->where("c.competencia >= :date_start and c.competencia <= :date_end and c.{$field} like :search and c.user = :user")
+                    ->setParameters(array(
+                        'search'=> "%{$search}%",
+                        'date_start'=>$base_date."-01",
+                        'date_end'=>$base_date."-31",
+                        'user' => $user
+                ));
+       
+        return $query->setFirstResult($firstResult)
+                        ->setMaxResults($maxResults)
+                        ->getQuery()
+                        ->getResult();
+    }
+
+    public function getRowsSearch($search, $field, $user = false) {
+        $session = $this->getSession();
+        $base_date = $session->get('baseDate');
+        $query = $this->createQueryBuilder('c')
+                ->select('Count(c)')
+                ->where("c.competencia >= :date_start and c.competencia <= :date_end and c.{$field} like :search and c.user = :user")
+                    ->setParameters(array(
+                        'search'=> "%{$search}%",
+                        'date_start'=>$base_date."-01",
+                        'date_end'=>$base_date."-31",
+                        'user' => $user
+                ));
+        
+        return $query->getQuery()
+                     ->getSingleScalarResult();
+    }
+
+    
+//    public function lancamentosGeralMes() {
+//        $session = new Session();
+//        $user = $session->get('user');
+//        $baseDate = $session->get('baseDate');
+//        $query = $this->createQueryBuilder('p')
+//                ->where('((p.vencimento >= :dateBaseIni and p.vencimento <= :dateBaseFim) or (p.vencimento < :dateBaseIni and p.pagamento is null) or (p.pagamento >= :dateBaseIni and p.pagamento <= :dateBaseFim)) and p.user = :user')
+//                ->setParameters(array(
+//                    'dateBaseIni'=>$baseDate."-01",
+//                    'dateBaseFim'=>$baseDate."-31",
+//                    'user' => $user->getId()
+//                ))
+//                ->orderBy('p.pagamento,p.vencimento', 'ASC')
+//                ->getQuery();
+//        //\Zend\Debug\Debug::dump($query->getSQL());
+//        //\Zend\Debug\Debug::dump($container->user->getId());
+//        return $query->getResult();
+//    }
 }
