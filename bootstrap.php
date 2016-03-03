347a59c26a4a5c3ddbeb486f92bbdf7a44fd01b0
Thiago Santos <thiago_vick@yahoo.com.br>
2016-03-03 18:10:53 -0300

    corrigindo chamado do autoloader

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 //error_reporting(0);
-require_once 'vendor\autoload.php';
+require_once __DIR__.'/vendor/autoload.php';
 
 use Doctrine\ORM\EntityManager,
     Doctrine\Common\EventManager as EventManager,
