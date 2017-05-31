713c73d58a291cf20d226e0b4d410301ed4ede50
Thiago Santos <thiago_vick@yahoo.com.br>
2017-05-31 15:15:08 -0300

    fix Competencia Validator

--- src/TVS/Base/Validator/Constraint/InvalidCompetenciaValidator.php
+++ src/TVS/Base/Validator/Constraint/InvalidCompetenciaValidator.php
@@ -6,21 +6,25 @@ use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
 
 class InvalidCompetenciaValidator extends ConstraintValidator {
+
     public function validate($value, Constraint $constraint) {
         if (!$this->checkDateByFormat($value)) {
             $this->context->buildViolation($constraint->message)
-                ->setParameter('%string%', $value)
-                ->addViolation();
+                    ->setParameter('%string%', $value)
+                    ->addViolation();
         }
     }
-    
-    public function checkDateByFormat($date, $format = 'm/Y') {
-        $t = \DateTime::createFromFormat($format, $date);
 
+    public function checkDateByFormat($date, $format = 'm/Y') {
+        if ($format == 'm/Y') {
+            $t = \DateTime::createFromFormat('d/m/Y', "01/" . $date);
+        } else {
+            $t = \DateTime::createFromFormat($format, $date);
+        }
         if ($t === false) {
             return false;
         }
-
         return $t->format($format) === $date;
     }
+
 }
